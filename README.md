ea048b5f2c55d20cdf2f2bb18b82a243c5c9aa1d
Vatsal Shah <shahvatsal90@gmail.com>
2016-04-19 15:41:31 +0530

    Updated docs

--- README.md
+++ README.md
@@ -1,2 +1,54 @@
-# bluedart
-Python based library for BlueDart SOAP API Integration
+Python BlueDart SOAP API Module
+===============================
+
+:Author: Vatsal Shah
+:Maintainer: Vatsal Shah
+:License: BSD
+:Status: Stable
+
+What is it?
+-----------
+
+A light wrapper around Bluedart's Webservice Soap API. I don't do much of any
+validation, but I'll help you sort through the pile of SOAP objects BlueDart
+uses.
+
+Installation
+------------
+
+The easiest way is via pip or easy_install::
+
+    pip install bluedart
+
+Quick Start
+-----------
+
+- Clone this repository.
+
+- Edit the `example_profile.py` file in See `examples/ <examples/>`_ with your bluedart credentials
+and run any of the provided examples.
+
+
+Support
+-------
+
+Issues & Questions: https://github.com/hornedbull/bluedart/issues
+
+Most problems are going to require investigation or a submitted 
+pull request.
+To contribute a new feature or service, feel free to create a pull request.
+We are always looking for new contributors to help maintain the project.
+
+
+Todos
+-----
+
+- Increase service specific request validation
+- Remove deprecated services (package movement service)
+
+Legal
+-----
+
+Copyright (C) 2008-2016 Vatsal Shah
+
+This software is licensed under the MIT License.
--- setup.py
+++ setup.py
@@ -2,12 +2,67 @@
 from setuptools import setup
 import bluedart
 
-LONG_DESCRIPTION = open('README.md').read()
+LONG_DESCRIPTION = """
+Python BlueDart SOAP API Module
+===============================
+
+:Author: Vatsal Shah
+:Maintainer: Vatsal Shah
+:License: BSD
+:Status: Stable
+
+What is it?
+-----------
+
+A light wrapper around Bluedart's Webservice Soap API. I don't do much of any
+validation, but I'll help you sort through the pile of SOAP objects BlueDart
+uses.
+
+Installation
+------------
+
+The easiest way is via pip or easy_install::
+
+    pip install bluedart
+
+Quick Start
+-----------
+
+- Clone this repository.
+
+- Edit the `example_profile.py` file in See `examples/ <examples/>`_ with your bluedart credentials
+and run any of the provided examples.
+
+
+Support
+-------
+
+Issues & Questions: https://github.com/hornedbull/bluedart/issues
+
+Most problems are going to require investigation or a submitted
+pull request.
+To contribute a new feature or service, feel free to create a pull request.
+We are always looking for new contributors to help maintain the project.
+
+
+Todos
+-----
+
+- Increase service specific request validation
+- Remove deprecated services (package movement service)
+
+Legal
+-----
+
+Copyright (C) 2008-2016 Vatsal Shah
+
+This software is licensed under the MIT License.
+"""
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Intended Audience :: Developers',
-    'License :: OSI Approved :: BSD License',
+    'License :: OSI Approved :: MIT License',
     'Natural Language :: English',
     'Operating System :: OS Independent',
     'Programming Language :: Python',
@@ -22,14 +77,13 @@ setup(name='bluedart',
       long_description=LONG_DESCRIPTION,
       author='Vatsal Shah',
       author_email='shahvatsal90@gmail.com',
-      maintainer='Python Bluedart Developers',
+      maintainer='Vatsal Shah',
       url='https://github.com/hornedbull/bluedart.git',
       download_url='http://pypi.python.org/pypi/bluedart/',
       packages=['bluedart'],
       package_dir={'bluedart': 'bluedart'},
-      package_data={'bluedart': ['wsdl/*.wsdl', 'wsdl/test_server_wsdl/*.wsdl']},
       platforms=['Platform Independent'],
-      license='BSD',
+      license='MIT',
       classifiers=CLASSIFIERS,
       keywords=KEYWORDS,
       requires=['suds'],
